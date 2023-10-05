bc049d39d99292fa2573c79f14d355bf987aa32c
saki4555 <saki999v@gmail.com>
2023-10-05 13:43:04 +0600

    fixed cors policy

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ async function run() {
 
         const toysCollection = client.db('marketplaceDb').collection('toys');
 
-        const indexKeys = { name: 1 };
-        const indexOptions = { name: "nameIndex" };
-        const result = await toysCollection.createIndex(indexKeys, indexOptions);
-        console.log(result);
+        // const indexKeys = { name: 1 };
+        // const indexOptions = { name: "nameIndex" };
+        // const result = await toysCollection.createIndex(indexKeys, indexOptions);
+        // console.log(result);
 
         app.get('/alltoys', async (req, res) => {
             const result = await toysCollection.find().limit(20).toArray();
@@ -118,7 +118,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
